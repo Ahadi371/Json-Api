878955caa7bf7fe8510030154e1aa818027775a1
Ahadi371 <mdahadi371@gmail.com>
2024-07-14 14:10:51 +0600

    Display Loaded Data

--- api.js
+++ api.js
@@ -1,5 +1,21 @@
-function loaData() {
+function loadData() {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
       .then(json => console.log(json))
+}
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => displayUsers(data))
+}
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => console.log(json)) 
+}
+function displayUsers(data) {
+    // console.log(data)
+    for (const user of data) {
+        console.log(user.name)
+    }
 }
