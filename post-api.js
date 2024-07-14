11a101b8538811b359d66122a33fa80a3239fd35
Ahadi371 <mdahadi371@gmail.com>
2024-07-14 17:29:28 +0600

    GET, POST

--- post-api.js
+++ post-api.js
@@ -19,3 +19,19 @@ function displayPost(posts) {
         postContainer.appendChild(div)
     }
 }
+function addApost() {
+fetch('https://jsonplaceholder.typicode.com/posts',{
+    method: 'POST',
+    body: JSON.stringify({
+        title: 'My new Post',
+        body: 'This is my post',
+        userId: 1
+    }),
+    headers:{
+        'Content-type': 'application/json; charset=UTF-8',
+
+    }
+}) 
+.then(res => res.json())
+.then(data => console.log(data))
+}
