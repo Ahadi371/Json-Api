12487ddc0696e84c8e797840838bf79b9ef5430b
Ahadi371 <mdahadi371@gmail.com>
2024-07-17 20:54:03 +0600

    countries appendChild

--- countries.js
+++ countries.js
@@ -9,7 +9,18 @@ const displayCountries = countries => {
 //     console.log(country)
     
 //   }
+const countriesDiv =document.getElementById('countries');    
 countries.forEach(country => {
-console.log(country)    
+console.log(country)  
+const div = document.createElement('div');
+// div.classList.add('country')
+const h3 = document.createElement('h3');
+h3.innerText =country.continents;
+
+div.appendChild(h3);
+const p =document.createElement('p');
+p.innerText = country.capital;
+div.appendChild(p);
+countriesDiv.appendChild(div); 
 });
 }
