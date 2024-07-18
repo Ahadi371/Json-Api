d250b2eb1589448f41e078ccf5268fa8e1b7658c
Ahadi371 <mdahadi371@gmail.com>
2024-07-18 19:40:16 +0600

    Call Dynamic API

--- countries.js
+++ countries.js
@@ -14,13 +14,17 @@ countries.forEach(country => {
 console.log(country)  
 const div = document.createElement('div');
 div.classList.add('country')
-const h3 = document.createElement('h3');
-h3.innerText =country.continents;
+div.innerHTML =`
+<h3>${country.continents}</h3>
+<p>${country.capital}</p>
+<button onclike="loadCountryByName">Details</button>`;
+// const h3 = document.createElement('h3');
+// h3.innerText =country.continents;
 
-div.appendChild(h3);
-const p =document.createElement('p');
-p.innerText = country.capital;
-div.appendChild(p);
+// div.appendChild(h3);
+// const p =document.createElement('p');
+// p.innerText = country.capital;
+// div.appendChild(p);
 countriesDiv.appendChild(div); 
 });
 }
