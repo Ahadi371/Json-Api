793d73cdc89ded3fe2b9f48c70e5e15e88ec3541
Ahadi371 <mdahadi371@gmail.com>
2024-08-12 13:04:18 +0600

    Display dynamic search result

--- mealbd.js
+++ mealbd.js
@@ -16,14 +16,14 @@ const displaySearchResult = meals =>{
         console.log(meal);
         const div =document.createElement('div');
         div.classList.add('col');
-        div.innerHTML =`<div class="card">
-            <img src="..." class="card-img-top" alt="...">
+        div.innerHTML =`<div class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">${meal.strInstructions.slice(0,200)}</p>
             </div>
           </div>`;
           searchResult.appendChild(div);
     })
-
+    
 }
