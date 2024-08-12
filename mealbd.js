155c1d49764fc9da70d95a36bef50e8170cab299
Ahadi371 <mdahadi371@gmail.com>
2024-08-12 12:58:23 +0600

    dainamip class added

--- mealbd.js
+++ mealbd.js
@@ -7,5 +7,23 @@ const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
 // console.log(url);
 fetch(url)
 .then(res =>res.json())
-.then(data =>console.log(data.meals))
+.then(data =>displaySearchResult(data.meals))
+}
+const displaySearchResult = meals =>{
+    // console.log(meals);
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        console.log(meal);
+        const div =document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML =`<div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>`;
+          searchResult.appendChild(div);
+    })
+
 }
