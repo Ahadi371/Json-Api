fe4f30ee4952bbd230b40786d6b11a28eb82f59b
Ahadi371 <mdahadi371@gmail.com>
2024-08-14 11:05:28 +0600

    Display dynamic search result and deteils

--- mealbd.js
+++ mealbd.js
@@ -28,10 +28,12 @@ const displaySearchResult = meals =>{
     
 }
 const loadMealDetail = mealId => {
+
+     
     console.log(mealId);
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
 // console.log(url);
 fetch(url)
 .then(res =>res.json())
-.then(data =>console.log(data))
+.then(data =>console.log(data.meals))
 }
