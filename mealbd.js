978dc425d7f02fa4a967e380f19ebe6aa1d0fe1d
Ahadi371 <mdahadi371@gmail.com>
2024-08-16 17:53:08 +0600

    Create dynamic url based on click and display data

--- mealbd.js
+++ mealbd.js
@@ -1,6 +1,7 @@
 const searchFood = () => {
     const searchField = document.getElementById('search-field')
 const searchText = searchField.value;
+// console.log(searchText)
 searchField.value = '';
 const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
 // console.log(url);
