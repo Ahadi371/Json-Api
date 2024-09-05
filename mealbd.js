bd4215310ce6f11fcc9e96c9cc2c7905b330b4b1
Ahadi371 <mdahadi371.email@example.com>
2024-09-05 17:40:21 +0600

    Mealbd changed

--- mealbd.js
+++ mealbd.js
@@ -29,6 +29,7 @@ const displaySearchResult = meals =>{
 const loadMealDetail = mealId => {
     console.log(mealId);
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+// console.log(url);
 fetch(url)
 .then(res =>res.json())
 .then(data => displayMealDetail(data.meals[0]))
